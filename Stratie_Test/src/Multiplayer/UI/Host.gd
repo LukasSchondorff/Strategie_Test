90cad61138a56c3fd8bd88232fa28262add445b0
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-08-04 19:16:02 +0200

    [MP] enable send button after hosting

--- Stratie_Test/src/Multiplayer/UI/Host.gd
+++ Stratie_Test/src/Multiplayer/UI/Host.gd
@@ -6,3 +6,4 @@ func _on_Host_button_up() -> void:
 	disabled = ret
 	get_node("../Leave").disabled = not ret
 	get_node("../Join").disabled = ret
+	get_node("../../MultiplayerChat/Lower/Send").disabled = false
