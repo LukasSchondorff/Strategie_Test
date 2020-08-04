0544b8c140410f73079bfd58175ced74f2af8754
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-08-04 17:29:00 +0200

    [MP] add script for chat

---
+++ Stratie_Test/src/Multiplayer/Chat.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public class Chat : Node
+{
+	private RichTextLabel ChatArea;
+	private Button SendButton;
+	private LineEdit MessageText;
+
+	public override void _Ready()
+	{
+		ChatArea = (RichTextLabel) GetNode("ChatArea");
+		MessageText = (LineEdit) GetNode("Lower/MessageText");
+		SendButton = (Button) GetNode("Lower/Send");
+
+		SendButton.Connect("button_up", this, nameof(SendMessage));
+	}
+
+	[Remote]
+	private void ReceiveMessage(string message)
+	{
+		ChatArea.AddText(message);
+		ChatArea.Newline();
+	}
+
+	private void SendMessage()
+	{
+		Rpc(nameof(ReceiveMessage), MessageText.Text);
+		MessageText.Clear();
+	}
+}
