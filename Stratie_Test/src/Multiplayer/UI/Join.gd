c101289411ee9fe72594a4027f8ea735e2c0cf15
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-08-04 16:40:38 +0200

    [MP] fix client name

--- Stratie_Test/src/Multiplayer/UI/Join.gd
+++ Stratie_Test/src/Multiplayer/UI/Join.gd
@@ -2,7 +2,7 @@ extends Button
 
 
 func _on_Join_button_up() -> void:
-	var ret = get_node("/root/Multiplayer_node").JoinGame(get_node("../Address").text, get_node("../Name"))
+	var ret = get_node("/root/Multiplayer_node").JoinGame(get_node("../Address").text, get_node("../Name").text)
 	disabled = ret
 	get_node("../Leave").disabled = not ret
 	get_node("../Host").disabled = ret
