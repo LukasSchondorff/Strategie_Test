454e58fb7c4983eecc91f5a9afb5be61e06be3bf
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-08-04 16:39:12 +0200

    [MP] fix missing client name

--- Stratie_Test/src/Multiplayer/UI/Join.gd
+++ Stratie_Test/src/Multiplayer/UI/Join.gd
@@ -2,7 +2,7 @@ extends Button
 
 
 func _on_Join_button_up() -> void:
-	var ret = get_node("/root/Multiplayer_node").JoinGame(get_node("../Address").text)
+	var ret = get_node("/root/Multiplayer_node").JoinGame(get_node("../Address").text, get_node("../Name"))
 	disabled = ret
 	get_node("../Leave").disabled = not ret
 	get_node("../Host").disabled = ret
