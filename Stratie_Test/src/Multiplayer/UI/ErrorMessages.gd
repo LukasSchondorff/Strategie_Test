bbd256697a3f3293bce198985e131ff8fe258052
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-08-05 14:24:48 +0200

    [MP] success handling

--- Stratie_Test/src/Multiplayer/UI/ErrorMessages.gd
+++ Stratie_Test/src/Multiplayer/UI/ErrorMessages.gd
@@ -2,6 +2,10 @@ extends RichTextLabel
 
 func _ready() -> void:
 	get_node("/root/Multiplayer_node").connect("ErrorSignal", self, "_on_Error")
+	get_node("/root/Multiplayer_node").connect("SuccessSignal", self, "_on_Success")
 
 func _on_Error(message: String) -> void:
 	bbcode_text = "[color=red]" + message + "[/color]"
+
+func _on_Success(message: String) -> void:
+	bbcode_text = "[color=green]" + message + "[/color]"
