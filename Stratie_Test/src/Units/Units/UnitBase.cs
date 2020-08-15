8019f77e8f10a801482ba3573a449b3e89fc1e74
Lukas Schondorff <l.schondorff@gmail.com>
2020-08-15 11:17:54 +0200

    [Unit] del unused functions

--- Stratie_Test/src/Units/Units/UnitBase.cs
+++ Stratie_Test/src/Units/Units/UnitBase.cs
@@ -1,31 +1,5 @@
 using Godot;
 using System;
 
-public class UnitBase  {
-    
-    private bool build = true;
-    
-    public void setbuild(bool build) {
-        this.build = build;
-    }
-    
-    public void switchbuild() {
-        this.build = !this.build;
-    } 
-    
-    public void mytest(Vector3 mouse_position, Vector3 cell_size) {
-        //GD.Print(mouse_position + " !!!");
-        mouse_position = new Vector3(mouse_position.x / cell_size.x, 
-                                     mouse_position.y / cell_size.y,
-                                     mouse_position.z / cell_size.z);
-        GD.Print(mouse_position);
-    }
-    
-    
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+public class UnitBase {
 }
