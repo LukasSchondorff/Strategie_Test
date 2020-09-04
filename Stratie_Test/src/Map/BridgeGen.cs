10e65cd0f0ded5fdedfb9c622bdb3d51bd837197
Felix Körner <56401181+ByteSquire@users.noreply.github.com>
2020-09-04 12:28:46 +0200

    [Map] [Bridge] arches with perfect orientation

--- Stratie_Test/src/Map/BridgeGen.cs
+++ Stratie_Test/src/Map/BridgeGen.cs
@@ -19,16 +19,16 @@ public class BridgeGen : Node
 		
 		bool bridge_shift = false;
 		bool reversed = false;
-		if (road_path[road_path.Count-1].y < road_path[0].y){
+		if (road_path[road_path.Count-1].y <= road_path[0].y){
 			road_path.Reverse();
 			reversed = true;
 			GD.Print("Reversed");
-			if(Math.Abs((road_path[road_path.Count-1].z - road_path[0].z)) % 2 == 0){
+			if(Math.Abs((road_path[road_path.Count-1].z - road_path[0].z)) % 2 != 0){
 				bridge_shift = true;
 			}
 		}
 		else{
-			if(Math.Abs((road_path[road_path.Count-1].x - road_path[0].x)) % 2 != 0){
+			if(Math.Abs((road_path[road_path.Count-1].x - road_path[0].x)) % 2 == 0){
 				bridge_shift = true;
 			}
 		}
@@ -91,30 +91,40 @@ public class BridgeGen : Node
 		GD.Print(direction, " | ", direction1);
 
 		for(int q = i; q < road_path.Count;){
-			if(q >= road_path.IndexOf(corner))
+			if(q >= road_path.IndexOf(corner)){
+				GenerateBridgeArch(road_path, road_path[q], bridge_shift, reversed, direction1, direction);
+				q = i;
+				/*
 				if (!GenerateBridgeArch(road_path, road_path[q], bridge_shift, reversed, direction1)){
 					q = i;
 				}
 				else{	
 					q += arch_length;
 				}
-			else
+				*/
+			}
+			else{
+				GenerateBridgeArch(road_path, road_path[q], bridge_shift, reversed, direction, direction1);
+				q = i;
+				/*
 				if (!GenerateBridgeArch(road_path, road_path[q], bridge_shift, reversed, direction)){
 					q = i;
 				}
 				else{
 					q += arch_length;
 				}
+				*/
+			}
 		}
 		road_path.Clear();
 	}
 
-	private bool GenerateBridgeArch(List<Vector3> road_path, Vector3 first, bool bridge_shift, bool reversed, int direction){
+	private bool GenerateBridgeArch(List<Vector3> road_path, Vector3 first, bool bridge_shift, bool reversed, int direction, int direction1){
 		int ypsilon_offset = -1;
 
 		if(i >= road_path.Count) return false;
 		int x, y, z;
-		
+
 		if(bridge_shift == true && first_arch){
 			first_arch = false;
 		}
@@ -123,7 +133,12 @@ public class BridgeGen : Node
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 0)return false;
+			if(y < 0)return false;
+
+			if(roadGen.GetCellItem(x, y - ypsilon_offset, z) == RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, y - ypsilon_offset, z) == RoadGen.CellItem.Corner2){
+				direction = direction1;
+			}
+
 			for(;y >= 0; y--){
 				roadGen.SetBridgerinoItem(x, y, z, RoadGen.CellItem.Pillar);
 			}
@@ -137,93 +152,178 @@ public class BridgeGen : Node
 		y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 		z = (int)road_path.ElementAt(i).z;
 		i++;
-		if(y <= 1)return false;
+		if(y < 0)return false;
 
 		if(direction == 0){
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(reversed)? 2 :*/ 8);//mirrored y-plane
-			
+			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 8);//mirrored y-plane
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
 			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Pillar);
-			
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(reversed)? 8 :*/ 2);//mirrored direction-plane
+			if(first.x != x && first.z != z){
+				if(direction1 == 1){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 8);//mirrored direction-plane
+				}
+				else if(direction1 == 2){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 18);//mirrored direction-plane
+				}
+				else{
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 20);//mirrored direction-plane
+				}
+			}
+			else{
+				roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 2);//mirrored direction-plane
+			}
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
 		}else if(direction == 1){
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(reversed)? 8 :*/ 2);//mirrored y-plane
-			
+			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 2);//mirrored y-plane
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
 			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Pillar);
-			
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(reversed)? 2 :*/ 8);//mirrored direction-plane
+			if(first.x != x && first.z != z){
+				if(direction1 == 0){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 2);//mirrored direction-plane
+				}
+				else if(direction1 == 2){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 18);//mirrored direction-plane
+				}
+				else{
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 20);//mirrored direction-plane
+				}
+			}
+			else{
+				roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 8);//mirrored direction-plane
+			}
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
 		}else if(direction == 2){
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(!reversed)?*/ 20 /*: 18*/);//mirrored y-plane
-			
+			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 20);//mirrored y-plane
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
 			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Pillar);
-			
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
-
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(!reversed)?*/ 18 /*: 20*/);//mirrored direction-plane
+			if(y < 0)return false;
+			
+			if(first.x != x && first.z != z){
+				if(direction1 == 0){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 2);//mirrored direction-plane
+				}
+				else if(direction1 == 1){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 8);//mirrored direction-plane
+				}
+				else{
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 20);//mirrored direction-plane
+				}
+			}
+			else{
+				roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 18);//mirrored direction-plane
+			}
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
 		}else if(direction == 3){
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(!reversed)?*/ 18 /*: 20*/);//mirrored y-plane
-			
+			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 18);//mirrored y-plane
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
 			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Pillar);
-			
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
+
 			if(i >= road_path.Count) return false;
 			x = (int)road_path[i].x;
 			y = (int)road_path.ElementAt(i).y + ypsilon_offset;
 			z = (int)road_path.ElementAt(i).z;
 			i++;
-			if(y <= 1)return false;
+			if(y < 0)return false;
 
-			roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, /*(!reversed)?*/ 20 /*: 18*/);//mirrored direction-plane
+			if(first.x != x && first.z != z){
+				if(direction1 == 0){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 2);//mirrored direction-plane
+				}
+				else if(direction1 == 1){
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 8);//mirrored direction-plane
+				}
+				else{
+					roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 18);//mirrored direction-plane
+				}
+			}
+			else{
+				roadGen.SetBridgerinoItem(x, (int)first.y+ypsilon_offset, z, RoadGen.CellItem.Slope_Arch_OverKopf, 20);//mirrored direction-plane
+			}
+			for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner1 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Corner2 && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Crossing && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.Slope && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.T && roadGen.GetCellItem(x, (int)first.y+b, z) != RoadGen.CellItem.StraightRoad; b++){
+				roadGen.SetBridgerinoItem(x, (int)first.y+b, z, RoadGen.CellItem.Pillar);
+			}
 		}
 
-		for(int b = 0; roadGen.GetCellItem(x, (int)first.y+b, z) == -1; b++){
-			//SetBridgerinoItem(x, (int)first.y+b, z, CellItem.Pillar);
-		}
 		return true;
 	}
 }
